170091581c900153af5f9e464eef5ac16fcb27b3
jiayan <1692363139@qq.com>
2020-07-27 19:49:50 +0800

    提交login

--- code/assets/js/login.js
+++ code/assets/js/login.js
@@ -7,4 +7,52 @@ $(function () {
         $(".login-box").show()
         $(".reg-box").hide()
     })
+    var layer = layui.layer
+    var form = layui.form
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function (value) {
+            var pwd = $('.reg-box [name="password"]').val()
+            if (value !== pwd) {
+                return '两次输入的密码不一致'
+            }
+        }
+    })
+    $('.reg-form').on('submit', function (e) {
+        e.preventDefault()
+        var data = {
+            username: $('.reg-form [name = username]').val(),
+            password: $('.reg-form [name = password]').val()
+        }
+        $.ajax({
+            method: 'POST',
+            url: '/api/reguser',
+            data: data,
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg('注册成功,请登录')
+                $('#link_login').click()
+            }
+        })
+    })
+    $('.login-form').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/api/login',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg('登录成功')
+                localStorage.setItem('token', res.token)
+                location.href = './index.html'
+            }
+        })
+    })
 })
