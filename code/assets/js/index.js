95fb21bbc97f324891a51a8b9820e700458c2006
jiayan <1692363139@qq.com>
2020-07-29 17:51:38 +0800

    提交index

---
+++ code/assets/js/index.js
@@ -0,0 +1,40 @@
+$(function () {
+    getUserInfo();
+    var layer = layui.layer
+    $('.btn_logout').on('click', function () {
+        layer.confirm('确定退出登录吗?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/code/login.html'
+            layer.close(index);
+        });
+    })
+
+    function getUserInfo() {
+        $.ajax({
+            method: "GET",
+            url: "/my/userinfo",
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg("获取用户信息失败");
+                }
+                renderAvatar(res.data);
+            },
+        });
+    }
+
+    function renderAvatar(user) {
+        var name = user.nickname || user.username;
+        $(".welcome").html("欢迎&nbsp;&nbsp;" + name);
+        if (user.user_pic !== null) {
+            $(".layui-nav-img").attr("src", user.user_pic).show();
+            $(".text-avatar").hide();
+        } else {
+            $(".layui-nav-img").hide();
+            $(".text-avatar").html(name[0].toUpperCase()).show();
+        }
+    }
+});
